18b956a3b2138cd1d3eb25ef1013434edecea5d7
hansun12 <songhs-0601@naver.com>
2021-12-22 18:46:37 +0900

    은행 프로젝트

--- account.c
+++ account.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-char account_num[20];
-char name[10];
-int money;
+#define SIZE 100
+#define ACCOUNT_SIZE 20
+
+char account_num[SIZE][ACCOUNT_SIZE];
+char name[SIZE][10];
+int money[SIZE];
+
+int auto_creater=1000000001; // 100-000-0001
+int head=0;
+
 
 void menu();
 
 void create_account();
-void show_account(char _name[]);
+void show_account(char _account_num[]);
 
 void deposit(char _name[]);
 void withdrawal(char _name[]);
@@ -44,13 +52,13 @@ void menu() {
             break;
 
         case 2:
-            printf("user name: ");
+            printf("account number: ");
             scanf("%s", temp_name);
             show_account(temp_name);
             break;
 
         case 3:
-            printf("user name: ");
+            printf("account number: ");
             scanf("%s", temp_name);
             deposit(temp_name);
             break;
@@ -75,73 +83,122 @@ void create_account() {
     int i;
 
     draw_line();
-    printf("input account number: ");
-    scanf("%s", account_num);
     
-    for(i=0; i < strlen(account_num); i++) {
-        if(!('0' < account_num[i] && account_num[i] < '9')) {
-            if(account_num[i] == '-') {
-                continue;
+    sprintf(account_num[head], "%d", auto_creater);
+
+    printf("input user name: ");
+    scanf("%s", name[head]);
+
+    money[head] = 0;
+
+    auto_creater++;
+    head++;
+
+    printf("\naccount create success!\n");
+    printf("your account number is <%s>\n", account_num[head-1]);
+}
+
+void show_account(char _account_num[]){
+    int i, j=0;
+    char temp[ACCOUNT_SIZE];
+    draw_line();
+
+    for(i = 0; i < strlen(_account_num); i++) {
+        if(!('0' <= _account_num[i] && _account_num[i] <= '9')) {
+            if(_account_num[i] != '-') {
+                printf("wrong input, only use[0]~[9] or [-]\n");
+                return;
             }
-            printf("wrong input, please input [0]~[9] or [-]\n");
-            return;
         }
     }
 
-    printf("input user name: ");
-    scanf("%s", name);
-
-    money = 0;
-
-    printf("\naccount create success!\n");
-}
-
-void show_account(char _name[]){
-    draw_line();
-    printf("<check account>\n");
-    if(!strcmp(_name, name)) {
-        printf("account number: %s\n", account_num);
-        printf("balance: %d\n", money);
-
-        printf("\ndone!\n");
-        return;
+    for(i = 0; i < strlen(_account_num); i++) {
+        if('0' <= _account_num[i] && _account_num[i] <= '9') {
+            temp[j++] = _account_num[i];
+        }
     }
 
-    printf("cannout found user <%s>\n",_name);
-}
-
-void deposit(char _name[]) {
-    int amount;
-
-    draw_line();
-    printf("<deposit>\n");
-    if(strcmp(_name, name)) {
-        printf("cannot access account<%s>\n", _name);
-        return;
+    for(i = 0; i < head; i++) {
+        if(!strcmp(temp, account_num[i])) {
+            printf("---\"%s\"'s account---\n", name[i]);
+            printf("amout: %d\n", money[i]);
+            return;
+        }
     }
-    printf("input price: ");
-    scanf("%d", &amount);
-    money += amount;
-    printf("\ndone!\n");
-    return;
-}
-
-void withdrawal(char _name[]){
-    int amount;
     
-    draw_line();
-    printf("<withdrawal>\n");
-    if(strcmp(_name, name)) {
-        printf("cannot access account<%s>\n", _name);
-        return;
+    printf("cannout found account <%s>.\n", _account_num);
+}
+
+void deposit(char _account_num[]) {
+    int i, j=0, value;
+    char temp[ACCOUNT_SIZE];
+    
+    for(i = 0; i < strlen(_account_num); i++) {
+        if(!('0' <= _account_num[i] && _account_num[i] <= '9')) {
+            if(_account_num[i] != '-') {
+                printf("wrong input, only use[0]~[9] or [-]\n");
+                return;
+            }//compare account number fine '-'
+        }
     }
-    printf("input price: ");
-    scanf("%d",&amount);
-    money -= amount;
-    printf("\ndone!\n");
+
+    for(i = 0; i < strlen(_account_num); i++) {
+        if('0' <= _account_num[i] && _account_num[i] <= '9') {
+            temp[j++] = _account_num[i];
+        }// save account num on temp
+    }
+
+    for(i = 0; i < head; i++) {
+        if(!strcmp(temp, account_num[i])) {
+            printf("input balance: ");
+            scanf("%d", &value);
+            money[i] += value;
+            draw_line();
+            return;
+        } 
+        // add money
+    }
+    printf("cannot found user <%s>.\n", _account_num);
+    draw_line();
     return;
+}
 
-
+void withdrawal(char _account_num[]){
+    int i, value, j=0;
+    char temp[ACCOUNT_SIZE];
+    draw_line();
+    for(i = 0; i < strlen(_account_num); i++) {
+        if(!('0' <= _account_num[i] && _account_num[i] <= '9')) {
+            if(_account_num[i] != '-') {
+                printf("wrong input, only use[0]~[9] or [-]\n");
+                return;
+            }
+        }
+    }
+    for(i = 0; i < strlen(_account_num); i++) {
+        if('0' <= _account_num[i] && _account_num[i] <= '9') {
+            temp[j++] = _account_num[i];
+        }
+    }
+    for(i = 0; i < head; i++) {
+        if(!strcmp(_account_num, account_num[i])) {
+            printf("input balance: ");
+            scanf("%d", &value);
+            if(money[i]>value){
+                money[i] -= value;
+                draw_line();
+                return;
+            }
+            else{
+                printf("no money!\n");
+                draw_line();
+                return;
+            }
+        } 
+    }
+    printf("cannot found user <%s>.\n", _account_num);
+    draw_line();
+    return;
 }
 
 void draw_line() {
